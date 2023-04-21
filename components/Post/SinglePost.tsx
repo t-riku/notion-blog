0ff0db94e1a33fff02b04618c1be6516b51e1d47
t-riku <dingariku@icloud.com>
2023-04-21 17:55:04 +0900

    ✨ 取得したポスト情報を展開し、一つのSinglePostコンポーネントとして表示

---
+++ components/Post/SinglePost.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+type Props = {
+  title: string;
+  description: string;
+  date: string;
+  tag: string;
+  slug: string;
+};
+
+const SinglePost = (props: Props) => {
+  const { title, description, date, tag, slug } = props;
+  return (
+    <>
+      <div>{title}</div>
+      <div>{description}</div>
+    </>
+  );
+};
+
+export default SinglePost;

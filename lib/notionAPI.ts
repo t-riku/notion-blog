35a868cf4744c275784c45334bf0dc9a26170e2a
t-riku <dingariku@icloud.com>
2023-04-21 17:53:59 +0900

    ✨ 新たにTagsを取得

--- lib/notionAPI.ts
+++ lib/notionAPI.ts
@@ -11,7 +11,7 @@ export const getAllPosts = async () => {
   });
 
   const allPosts = posts.results;
-
+  //   return allPosts;
   //   一つ一つのポストを展開
   return allPosts.map((post) => {
     return getPageMeataData(post);
@@ -19,11 +19,18 @@ export const getAllPosts = async () => {
 };
 
 const getPageMeataData = (post) => {
+  const getTags = (tags) => {
+    const allTags = tags.map((tag) => {
+      return tag.name;
+    });
+    return allTags;
+  };
   return {
     id: post.id,
-    title: post.properties.名前.title[0].plain_text,
+    title: post.properties.Name.title[0].plain_text,
     description: post.properties.Description.rich_text[0].plain_text,
     date: post.properties.Date.date.start,
     slug: post.properties.Slug.rich_text[0].plain_text,
+    tags: getTags(post.properties.Tags.multi_select),
   };
 };
